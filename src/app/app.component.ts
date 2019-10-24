e42ac30cae575e28eab7ba9150b3e34d94bfec51
vakhsakhalyan <vakhsakhalyan@gmail.com>
2019-10-24 17:17:36 +0400

    adding new styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'weather';
-  darkModeActived: any;
+  darkModeActived: boolean;
+  showMenu: boolean;
+
+  themeChange(evt) {
+    this.darkModeActived = evt.target.checked;
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
